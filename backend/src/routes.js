fca94c7211758d66b7d10e3534c30d3ebb2492e2
ThalesLoreto <thalesmulti3@gmail.com>
2019-10-03 23:13:29 -0300

    create DashboardController && Booking store method

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,6 +3,8 @@ const multer = require('multer');
 
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const DashBoardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const uploadConfig = require('./config/upload');
 
@@ -13,5 +15,9 @@ routes.post('/sessions', SessionController.store);
 
 routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+routes.get('/dashboard', DashBoardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
