bebede15841264038e046850a6e6378e63ed9d56
ThalesLoreto <thalesmulti3@gmail.com>
2019-10-04 19:19:14 -0300

    real time in mobile && finish app

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,8 @@ const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
 const DashBoardController = require('./controllers/DashboardController');
 const BookingController = require('./controllers/BookingController');
+const ApprovalController = require('./controllers/ApprovalController');
+const RejectionController = require('./controllers/RejectionController');
 
 const uploadConfig = require('./config/upload');
 
@@ -19,5 +21,8 @@ routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 routes.get('/dashboard', DashBoardController.show);
 
 routes.post('/spots/:spot_id/bookings', BookingController.store);
+
+routes.post('/bookings/:booking_id/approvals', ApprovalController.store);
+routes.post('/bookings/:booking_id/rejections', RejectionController.store);
 
 module.exports = routes;
